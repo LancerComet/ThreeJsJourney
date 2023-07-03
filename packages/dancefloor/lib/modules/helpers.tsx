ae0e9c36f831c39b7acfe33cc92cb07d225088ac
LancerComet <chw644@hotmail.com>
2023-07-04 02:06:44 +0800

    + Add InfiniteGridHelper.

--- packages/dancefloor/lib/modules/helpers.tsx
+++ packages/dancefloor/lib/modules/helpers.tsx
@@ -147,6 +147,93 @@ const HemisphereLightHelper = defineComponent({
   }
 })
 
+const InfiniteGridHelper = defineComponent({
+  name: 'InfiniteGridHelper',
+  props: {
+    size1: {
+      type: Number as PropType<number>,
+      default: 2
+    },
+    size2: {
+      type: Number as PropType<number>,
+      default: 10
+    },
+    color: {
+      type: [Object, Number, String] as PropType<THREE.ColorRepresentation>,
+      default: 0xffffff
+    },
+    distance: {
+      type: Number as PropType<number>,
+      default: 8000
+    }
+  },
+  setup (props) {
+    const { size1, size2, color, distance } = props
+    const axes = 'xyz'
+    const planeAxes = axes.substr(0, 2)
+    const geometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+
+    const material = new THREE.ShaderMaterial({
+      side: THREE.DoubleSide,
+      uniforms: {
+        uSize1: { value: size1 },
+        uSize2: { value: size2 },
+        uColor: { value: new THREE.Color(color) },
+        uDistance: { value: distance }
+      },
+      transparent: true,
+      vertexShader: `
+          varying vec3 worldPosition;
+          uniform float uDistance;
+          void main() {
+            vec3 pos = position.${axes} * uDistance;
+            pos.${planeAxes} += cameraPosition.${planeAxes};
+            worldPosition = pos;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+          }`,
+      fragmentShader: `
+          varying vec3 worldPosition;
+          uniform float uSize1;
+          uniform float uSize2;
+          uniform vec3 uColor;
+          uniform float uDistance;
+          float getGrid(float size) {
+            vec2 r = worldPosition.${planeAxes} / size;
+            vec2 grid = abs(fract(r - 0.5) - 0.5) / fwidth(r);
+            float line = min(grid.x, grid.y);
+            return 1.0 - min(line, 1.0);
+          }
+          void main() {
+            float d = 1.0 - min(distance(cameraPosition.${planeAxes}, worldPosition.${planeAxes}) / uDistance, 1.0);
+            float g1 = getGrid(uSize1);
+            float g2 = getGrid(uSize2);
+            gl_FragColor = vec4(uColor.rgb, mix(g2, g1, g1) * pow(d, 3.0));
+            gl_FragColor.a = mix(0.5 * gl_FragColor.a, gl_FragColor.a, g2);
+            if (gl_FragColor.a <= 0.0) discard;
+          }`,
+      extensions: { derivatives: true }
+    })
+
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.frustumCulled = false
+    mesh.rotation.x = Math.PI / 2
+
+    const container = injectContainer()
+    container?.add(mesh)
+
+    onBeforeUnmount(() => {
+      geometry.dispose()
+      material.dispose()
+      mesh.clear()
+      mesh.removeFromParent()
+    })
+
+    return () => (
+      <div class='infinite-grid-helper' />
+    )
+  }
+})
+
 const RectAreaLightHelper = defineComponent({
   name: 'RectAreaLightHelper',
 
@@ -198,5 +285,6 @@ export {
   AxesHelper,
   GridHelper,
   HemisphereLightHelper,
+  InfiniteGridHelper,
   RectAreaLightHelper
 }
